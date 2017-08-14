3dd4ffbdb9dcec9d86e5295e46abdfe0a94cded2
Michael Ogala <michael.ogala@andela.com>
2017-08-14 04:06:29 +0100

    Add show route

--- app/models/friend.js
+++ app/models/friend.js
@@ -4,5 +4,8 @@ export default DS.Model.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
   email: DS.attr('string'),
-  twitter: DS.attr('string')
+  twitter: DS.attr('string'),
+  fullName: Ember.computed('firstName', 'lastName', function() {
+    return `${this.get('firstName')} ${this.get('lastName')}`;
+  })
 });
