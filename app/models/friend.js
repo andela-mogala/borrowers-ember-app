4f4942232fbc696fd15eb1d20add0b165b2e09c0
Michael Ogala <michael.ogala@andela.com>
2017-08-15 00:27:09 +0100

    display all loans on friend profile

--- app/models/friend.js
+++ app/models/friend.js
@@ -6,6 +6,7 @@ export default DS.Model.extend({
   lastName: DS.attr('string'),
   email: DS.attr('string'),
   twitter: DS.attr('string'),
+  loans: DS.hasMany('loan'),
   fullName: Ember.computed('firstName', 'lastName', function() {
     return `${this.get('firstName')} ${this.get('lastName')}`;
   })
