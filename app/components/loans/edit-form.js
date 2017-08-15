ed10deb598ccbefc1d717c5df1dbefd860659cd4
Michael Ogala <michael.ogala@andela.com>
2017-08-15 14:41:20 +0100

    Add ability to loan an article

--- app/components/loans/edit-form.js
+++ app/components/loans/edit-form.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
+  articles: Ember.computed(function() {
+    return this.get('store').findAll('article');
+  }).readOnly(),
+
+  actions: {
+    cancel() {
+      this.back(this.get('model.friend'));
+    },
+    save() {
+      this.get('model').save()
+        .then((loan) => {
+          this.back(loan.get('friend'))
+        })
+        .catch(() => {
+          this.set('errorMessage', 'There was something wrong saving the loan.');
+        });
+    }
+  }
 });
