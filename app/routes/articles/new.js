03c3c7090708eac1509b78890131dcaba50a3dca
Michael Ogala <michael.ogala@andela.com>
2017-08-15 17:32:51 +0100

    clean up unmodified records on route exit

--- app/routes/articles/new.js
+++ app/routes/articles/new.js
@@ -3,5 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.createRecord('article');
+  },
+
+  resetController(controller, isExiting) {
+    if (isExiting) {
+      const model = controlller.get('model');
+      if (model.get('isNew')) {
+        model.destroyRecord();
+      }
+    }
   }
 });
--- app/routes/loans/new.js
+++ app/routes/loans/new.js
@@ -5,5 +5,14 @@ export default Ember.Route.extend({
     return this.store.createRecord('loan', {
       friend: this.modelFor('friends/show')
     });
+  },
+
+  resetController(controller, isExiting) {
+    if (isExiting) {
+      const model = controller.get('model');
+      if (model.get('isNew')) {
+        model.destroyRecord();
+      }
+    }
   }
 });
