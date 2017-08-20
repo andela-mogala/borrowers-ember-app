e5a5cb3385588c2c569e910c3a40e1283710e3fc
Michael Ogala <michael.ogala@andela.com>
2017-08-20 12:49:03 +0100

    acceptance for friends

--- mirage/factories/friend.js
+++ mirage/factories/friend.js
@@ -2,7 +2,7 @@ import { Factory, faker } from 'ember-cli-mirage';
 
 export default Factory.extend({
   firstName() {
-    return faker.name.fristName();
+    return faker.name.firstName();
   },
   lastName() {
     return faker.name.lastName();
---
+++ tests/acceptance/friends-test.js
@@ -0,0 +1,17 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'borrowers/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | friends', {
+  beforeEach() {
+    server.createList('friend', 10);
+  }
+});
+
+test('visiting /friends', function(assert) {
+  visit('/friends');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/friends');
+    assert.equal(find('table tbody tr').length, 10, 'display 10 friends');
+  });
+});
